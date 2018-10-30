651ef02bb68f4b9177c185ebf17e251140e412c6
Alexandra Johnson <alexandra@queryclick.com>
2018-10-30 18:52:02 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,19 +1,20 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-let index = 0;
 
 function init() {
   document.addEventListener('keydown',onKeyDownHandler)
 }
+
+let index = 0;
 
 function onKeyDownHandler(e) {
   const key = parseInt(e.detail || e.which);
   console.log(key)
   console.log(index)
   
-  if (key === alphabet[index]) {
+  if (key === code[index]) {
     index++;
  
-    if (index === alphabet.length) {
+    if (index === code.length) {
       alert("Hurray!");
  
       index = 0;

606ba0412b0a49eadf7ae7dc918643a5b64c95c4
Alexandra Johnson <alexandra@queryclick.com>
2018-10-30 18:42:26 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,9 +1,5 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 
-function init() {
- document.body.addEventListener('keydown', function(event)
- 
-})
-}
-
+const input = document.querySelector('input')
+input.addEventListener('keydown', e => console.log(e.which))
